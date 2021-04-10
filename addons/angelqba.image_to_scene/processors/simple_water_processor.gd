004fe8aa276fa9776e40a84843de81fbb0102f6f
Angel <angelqba@gmail.com>
2021-04-09 22:18:28 -0400

    -improved water processor.

--- addons/angelqba.image_to_scene/processors/simple_water_processor.gd
+++ addons/angelqba.image_to_scene/processors/simple_water_processor.gd
@@ -18,6 +18,27 @@ func process(layers, current_layer, current_result, selected_node):
 	# do parent process
 	var result = .process(layers, current_layer, current_result, selected_node)
 	
+	var min_y = null
+	
+	# search for min y value of previous layer
+	for z in range(0, height):
+		for x in range(0, width):
+			var no_previous_point = false
+			for pg in previous_layer['point_groups']:
+				for result_pg in result['point_groups']:
+					if pg[x][z] and result_pg[x][z]:
+#						var tmp = pg[x][z]['vector'].y
+#						pg[x][z]['vector'].y -= result_pg[x][z]['vector'].y
+#						result_pg[x][z]['vector'].y = tmp
+						if min_y == null or min_y > pg[x][z]['vector'].y:
+							min_y = pg[x][z]['vector'].y
+							
+					else:
+						no_previous_point = true
+
+			if no_previous_point:
+				continue
+	
 	# modify previous layer, lowering it
 	for z in range(0, height):
 		for x in range(0, width):
@@ -25,9 +46,9 @@ func process(layers, current_layer, current_result, selected_node):
 			for pg in previous_layer['point_groups']:
 				for result_pg in result['point_groups']:
 					if pg[x][z] and result_pg[x][z]:
-						var tmp = pg[x][z]['vector'].y
-						pg[x][z]['vector'].y -= result_pg[x][z]['vector'].y
-						result_pg[x][z]['vector'].y = tmp
+#						var tmp = pg[x][z]['vector'].y
+						pg[x][z]['vector'].y = min_y - result_pg[x][z]['vector'].y
+						result_pg[x][z]['vector'].y = min_y
 					else:
 						no_previous_point = true
 
